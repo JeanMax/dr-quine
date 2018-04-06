92cba8eb20e45218c3f36361b8d3d59f24efbca5
JeanMax <mcanal@student.42.fr>
2018-04-06 04:05:50 +0200

    Fix Colleen asm II

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -22,7 +22,7 @@ prolog:
 
 epilog:
     mov eax, YAY
-    leave
+    pop rbp
     ret
 
 main:
@@ -31,14 +31,15 @@ main:
 ;
     call prolog
 
-    lea rdi, [rel code]
-    mov rsi, ENDL
-    mov rdx, QUOTE
-    lea rcx, [rel code]
+    lea edi, [rel code]
+    mov esi, ENDL
+    mov edx, QUOTE
+    lea ecx, [rel code]
+    mov eax, YAY
     call printf
 
     call epilog
     ret
 
 section .data
-    code db ";%1$c;    Colleen.s%1$c;%1$c%1$c%%define QUOTE 34%1$c%%define ENDL 10%1$c%%define YAY 0%1$c%1$csection .text%1$c    global start%1$c    global main%1$c    extern printf%1$c%1$cstart:%1$c    call main%1$c    ret%1$c%1$cprolog:%1$c    push rbp%1$c    mov rbp, rsp%1$c    ret%1$c%1$cepilog:%1$c    mov eax, YAY%1$c    leave%1$c    ret%1$c%1$cmain:%1$c;%1$c;    this is the tricky part%1$c;%1$c    call prolog%1$c%1$c    lea rdi, [rel code]%1$c    mov rsi, ENDL%1$c    mov rdx, QUOTE%1$c    lea rcx, [rel code]%1$c    call printf%1$c%1$c    call epilog%1$c    ret%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c"
+    code db ";%1$c;    Colleen.s%1$c;%1$c%1$c%%define QUOTE 34%1$c%%define ENDL 10%1$c%%define YAY 0%1$c%1$csection .text%1$c    global start%1$c    global main%1$c    extern printf%1$c%1$cstart:%1$c    call main%1$c    ret%1$c%1$cprolog:%1$c    push rbp%1$c    mov rbp, rsp%1$c    ret%1$c%1$cepilog:%1$c    mov eax, YAY%1$c    pop rbp%1$c    ret%1$c%1$cmain:%1$c;%1$c;    this is the tricky part%1$c;%1$c    call prolog%1$c%1$c    lea edi, [rel code]%1$c    mov esi, ENDL%1$c    mov edx, QUOTE%1$c    lea ecx, [rel code]%1$c    mov eax, YAY%1$c    call printf%1$c%1$c    call epilog%1$c    ret%1$c%1$csection .data%1$c    code db %2$c%3$s%2$c%1$c"
