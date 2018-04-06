ff2525e110fca3a1a89b39bb55b26a74715a34b9
JeanMax <mcanal@student.42.fr>
2018-04-06 23:06:39 +0200

    Add stupid defines in Grace

--- PY/Grace/Grace.py
+++ PY/Grace/Grace.py
@@ -4,8 +4,10 @@
 #   Grace.py
 #
 
-CODE='#!/usr/bin/python%c%c#%c#   Grace.py%c#%c%cCODE=%c%s%c%c%cf=open(%cGrace_kid.py%c, %cw%c)%cf.write(CODE %c (10, 10, 10, 10, 10, 10, 39, CODE, 39, 10, 10, 39, 39, 39, 39, 10, 37, 10, 10))%cf.close()%c'
+USEFUL_DEFINE1=42
+USEFUL_DEFINE2=42
+CODE='#!/usr/bin/python%c%c#%c#   Grace.py%c#%c%cUSEFUL_DEFINE1=42%cUSEFUL_DEFINE2=42%cCODE=%c%s%c%c%cf=open(%cGrace_kid.py%c, %cw%c)%cf.write(CODE %c (10, 10, 10, 10, 10, 10, 10, 10, 39, CODE, 39, 10, 10, 39, 39, 39, 39, 10, 37, 10, 10))%cf.close()%c'
 
 f=open('Grace_kid.py', 'w')
-f.write(CODE % (10, 10, 10, 10, 10, 10, 39, CODE, 39, 10, 10, 39, 39, 39, 39, 10, 37, 10, 10))
+f.write(CODE % (10, 10, 10, 10, 10, 10, 10, 10, 39, CODE, 39, 10, 10, 39, 39, 39, 39, 10, 37, 10, 10))
 f.close()
